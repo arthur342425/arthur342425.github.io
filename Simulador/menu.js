cefc39a314b6e82edffa9dda6b9ae5591d242efc
Arthur Bento Silva <130567247+arthur342425@users.noreply.github.com>
2024-06-30 21:47:23 -0300

    Upload de simulador

---
+++ Simulador/menu.js
@@ -0,0 +1,12 @@
+function function01(){
+   var creditos = document.querySelector('.credi')
+    var x = document.querySelector('i')
+   creditos.classList.add("show")
+   x.classList.add("show")
+}
+function function02(){
+    var creditos = document.querySelector('.credi')
+     var x = document.querySelector('i')
+    creditos.classList.remove("show")
+    x.classList.remove("show")
+ }
